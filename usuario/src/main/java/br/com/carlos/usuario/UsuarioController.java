266373c5f70505763daad3c813e95947a18ebbaf
Carlos-Henrique93 <carlos.henriquesantos@hotmail.com>
2025-08-27 23:20:45 -0300

    feat: Inserido o UsuarioRepository, inserção da dependência do MySQL e melhorando o UsuarioController

--- usuario/src/main/java/br/com/carlos/usuario/UsuarioController.java
+++ usuario/src/main/java/br/com/carlos/usuario/UsuarioController.java
@@ -1,29 +1,27 @@
 package br.com.carlos.usuario;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.carlos.usuario.UsuarioRepository.UsuarioRepository;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-public class UsuarioController {
 
-    private final List<Usuario> usuarios = new ArrayList<>();
+    @RestController
+    @RequestMapping("/usuarios") // ← atenção aqui
+    public class UsuarioController {
 
-    @PostMapping
-    public Usuario criarUsuario(@RequestBody Usuario usuario) {
-        usuario.setId((long) (usuarios.size() + 1)); // gera ID simples
-        usuarios.add(usuario);
-        return usuario;
-    }
+        private final UsuarioRepository usuarioRepository;
 
-    @GetMapping("/hello")
-    public String HelloWord(){
-        return "Hello World!";
-    }
+        public UsuarioController(UsuarioRepository usuarioRepository) {
+            this.usuarioRepository = usuarioRepository;
+        }
+
+        @PostMapping
+        public Usuario criarUsuario(@RequestBody Usuario usuario) {
+            return usuarioRepository.save(usuario);
+        }
+
 }
